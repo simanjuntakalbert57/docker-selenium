4bcd30cd9c916eb2fa8d82a67e26a233709facf5
Albert Simanjuntak <albertsimanjuntak@Alberts-MacBook-Air.local>
2025-07-06 20:13:21 +0700

    web automation

--- src/main/java/com/demo/base/BaseTest.java
+++ src/main/java/com/demo/base/BaseTest.java
@@ -3,12 +3,15 @@ package com.demo.base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
     public WebDriver driver;
 
     public void setUp(){
         // Setup WebDriver
-        System.setProperty("webdriver.chrome.drive", "/Users/bytedance/CourseQAAutomation/APIAdvance/bootcamp_batch3/materi/automation-after-office-batch3/chromedriver");
+        // System.setProperty("webdriver.chrome.drive", "/Users/bytedance/CourseQAAutomation/APIAdvance/bootcamp_batch3/materi/automation-after-office-batch3/chromedriver");
+        WebDriverManager.chromedriver().setup();  // ini auto download + setup
         driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/client");
         driver.manage().window().maximize();
